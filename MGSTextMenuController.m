vakoc/Fragaria

commit 88a90236efaed05d0cf6e6f28b0576f1d86785bc

Reimplement line endings code to be more solid. Now all unicode-defined line endings are properly handled.

SMLTextView+MGSTextActions.m
@@ -821,7 +821,7 @@
         NSRange selectedRange = NSMakeRange([item rangeValue].location - sumOfDeletedLineEndings, [item rangeValue].length);
         NSString *stringToRemoveLineEndingsFrom = [text substringWithRange:selectedRange];
         NSInteger originalLength = [stringToRemoveLineEndingsFrom length];
-        NSString *stringWithNoLineEndings = [SMLText removeAllLineEndingsInString:stringToRemoveLineEndingsFrom];
+        NSString *stringWithNoLineEndings = [self removeAllLineEndingsFromString:stringToRemoveLineEndingsFrom];
         NSInteger newLength = [stringWithNoLineEndings length];
         if ([self shouldChangeTextInRange:NSMakeRange(selectedRange.location, originalLength) replacementString:stringWithNoLineEndings]) { // Do it this way to mark it as an Undo
             [self replaceCharactersInRange:NSMakeRange(selectedRange.location, originalLength) withString:stringWithNoLineEndings];
@@ -836,6 +836,21 @@
         [self setSelectedRanges:updatedSelectionsArray];
     }
 }
+
+
+- (NSString *)removeAllLineEndingsFromString:(NSString*)orig
+{
+    NSMutableString *string = [orig mutableCopy];
+    NSCharacterSet *newlines = [NSCharacterSet newlineCharacterSet];
+    NSRange range;
+    
+    while ((range = [string rangeOfCharacterFromSet:newlines]).length) {
+        [string replaceCharactersInRange:range withString:@""];
+    }
+    return string;
+}
+
+
 
 /*
  
@@ -853,7 +868,7 @@
     
     NSRange selectedRange = [self selectedRange];
     NSString *text = [self string];
-    NSString *convertedString = [SMLText convertLineEndings:text inDocument:document];
+    NSString *convertedString = [self convertLineEndings:text];
     [self replaceCharactersInRange:NSMakeRange(0, [text length]) withString:convertedString];
     [self setSelectedRange:selectedRange];
 }
@@ -874,9 +889,52 @@
     
     NSRange selectedRange = [self selectedRange];
     NSString *text = [self string];
-    NSString *convertedString = [SMLText convertLineEndings:text inDocument:document];
+    NSString *convertedString = [self convertLineEndings:text];
     [self replaceCharactersInRange:NSMakeRange(0, [text length]) withString:convertedString];
     [self setSelectedRange:selectedRange];
+}
+
+
+/*
+ 
+ - convertLineEndings:inDocument:
+ 
+ */
+- (NSString *)convertLineEndings:(NSString *)stringToConvert
+{
+    NSInteger lineEndings;
+    NSString *newLineEnding;
+    id document = [fragaria docSpec];
+    
+    if ([[document valueForKey:@"lineEndings"] integerValue] == 0) { // It hasn't been changed by the user so use the one from the defaults
+        lineEndings = [[SMLDefaults valueForKey:@"LineEndingsPopUp"] integerValue] + 1;
+    } else {
+        lineEndings = [[document valueForKey:@"lineEndings"] integerValue];
+    }
+    
+    if (lineEndings == SMLLeaveLineEndingsUnchanged) {
+        return stringToConvert;
+    }
+    
+    NSMutableString *returnString = [NSMutableString stringWithString:stringToConvert];
+    
+    
+    if (lineEndings == SMLDarkSideLineEndings) { // CRLF
+        newLineEnding = @"\r\n";
+    } else if (lineEndings == SMLMacLineEndings) { // CR
+        newLineEnding = @"\r";
+    } else { // LF
+        newLineEnding = @"\n";
+    }
+    
+    NSRange range;
+    NSCharacterSet *newlines = [NSCharacterSet newlineCharacterSet];
+    
+    while ((range = [returnString rangeOfCharacterFromSet:newlines]).length) {
+        [returnString replaceCharactersInRange:range withString:newLineEnding];
+    }
+    
+    return returnString;
 }
 
 

commit 297bd591443b4cc2004bfb2145f4d232a81d5669

Remove line ending change actions.
Line ending changing is useful, but the existing implementation is untested (and probably broken after my rewrite in 88a9023) and the docspec properties it relies on are not exported. Maybe I'll reimplement that in the future.

MGSTextMenuController.m
@@ -329,7 +329,7 @@ static id sharedInstance = nil;
  */
 - (IBAction)changeLineEndingsAction:(id)sender
 {
-    [NSApp sendAction:@selector(changeLineEndings:) to:nil from:sender];
+    NSLog(@"This action is deprecated and has no effect.");
 }
 
 

SMLTextView+MGSTextActions.h
@@ -29,7 +29,6 @@
 - (IBAction)closeTag:(id)sender;
 - (IBAction)commentOrUncomment:(id)sender;
 - (IBAction)removeLineEndings:(id)sender;
-- (IBAction)changeLineEndings:(id)sender;
 - (IBAction)prepareForXML:(id)sender;
 - (IBAction)transpose:(id)sender;
 

SMLTextView+MGSTextActions.m
@@ -875,90 +875,5 @@
     return string;
 }
 
-/*
- 
- - changeLineEndingsAction:
- 
- */
-- (IBAction)changeLineEndings:(id)sender
-{
-    id document = [fragaria docSpec];
-    
-    [[[document valueForKey:ro_MGSFOSyntaxColouring] undoManager] registerUndoWithTarget:self selector:@selector(performUndoChangeLineEndings:) object:[NSArray arrayWithObject:[document valueForKey:@"lineEndings"]]];
-    [[[document valueForKey:ro_MGSFOSyntaxColouring] undoManager] setActionName:NAME_FOR_UNDO_CHANGE_LINE_ENDINGS];
-    
-    [document setValue:[NSNumber numberWithInteger:[sender tag] - 150] forKey:@"lineEndings"];
-    
-    NSRange selectedRange = [self selectedRange];
-    NSString *text = [self string];
-    NSString *convertedString = [self convertLineEndings:text];
-    [self replaceCharactersInRange:NSMakeRange(0, [text length]) withString:convertedString];
-    [self setSelectedRange:selectedRange];
-}
-
-/*
- 
- - performUndoChangeLineEndings:
- 
- */
-- (void)performUndoChangeLineEndings:(id)sender
-{
-    id document = [fragaria docSpec];
-    
-    [[[document valueForKey:ro_MGSFOSyntaxColouring] undoManager] registerUndoWithTarget:self selector:@selector(performUndoChangeLineEndings:) object:[NSArray arrayWithObject:[document valueForKey:@"lineEndings"]]];
-    [[[document valueForKey:ro_MGSFOSyntaxColouring] undoManager] setActionName:NAME_FOR_UNDO_CHANGE_LINE_ENDINGS];
-    
-    [document setValue:[sender objectAtIndex:0] forKey:@"lineEndings"];
-    
-    NSRange selectedRange = [self selectedRange];
-    NSString *text = [self string];
-    NSString *convertedString = [self convertLineEndings:text];
-    [self replaceCharactersInRange:NSMakeRange(0, [text length]) withString:convertedString];
-    [self setSelectedRange:selectedRange];
-}
-
-
-/*
- 
- - convertLineEndings:inDocument:
- 
- */
-- (NSString *)convertLineEndings:(NSString *)stringToConvert
-{
-    NSInteger lineEndings;
-    NSString *newLineEnding;
-    id document = [fragaria docSpec];
-    
-    if ([[document valueForKey:@"lineEndings"] integerValue] == 0) { // It hasn't been changed by the user so use the one from the defaults
-        lineEndings = [[SMLDefaults valueForKey:@"LineEndingsPopUp"] integerValue] + 1;
-    } else {
-        lineEndings = [[document valueForKey:@"lineEndings"] integerValue];
-    }
-    
-    if (lineEndings == SMLLeaveLineEndingsUnchanged) {
-        return stringToConvert;
-    }
-    
-    NSMutableString *returnString = [NSMutableString stringWithString:stringToConvert];
-    
-    
-    if (lineEndings == SMLDarkSideLineEndings) { // CRLF
-        newLineEnding = @"\r\n";
-    } else if (lineEndings == SMLMacLineEndings) { // CR
-        newLineEnding = @"\r";
-    } else { // LF
-        newLineEnding = @"\n";
-    }
-    
-    NSRange range;
-    NSCharacterSet *newlines = [NSCharacterSet newlineCharacterSet];
-    
-    while ((range = [returnString rangeOfCharacterFromSet:newlines]).length) {
-        [returnString replaceCharactersInRange:range withString:newLineEnding];
-    }
-    
-    return returnString;
-}
-
 
 @end