06aa348c3790759b3a026f5d50bd5a675ef0262b
Jim Derry <balthisar@gmail.com>
2015-02-15 21:06:44 +0800

    Document SMLSyntaxError.h. Deprecate some properties in order to add new, renamed properties. Removed useless ivars. Synthesized underscored ivars. Can specify own image in addition to using one of the defaults.

--- SMLSyntaxError.m
+++ SMLSyntaxError.m
@@ -10,26 +10,134 @@
 
 @implementation SMLSyntaxError
 
-@synthesize line, character, code, length, description, hideWarning, customBackgroundColor, warningStyle;
+// automatic
+@synthesize line, character, length, description, hidden, warningLevel;
 
+// manual
+@synthesize errorLineHighlightColor = _errorLineHighlightColor;
+@synthesize errorBackgroundHighlightColor = _errorBackgroundHighlightColor;
+@synthesize errorForegroundHilightColor = _errorForegroundHilightColor;
+@synthesize warningImage = _warningImage;
+
+// to be deprecated
+@synthesize code; // deprecated; separate line as reminder to remove.
 
 #pragma mark - Class Methods
 
++ (NSImage *)defaultImageForWarningLevel:(MGSErrorType)level
+{
+    // Note these are in order by MGSErrorType. @todo: Bounds checking.
+    NSArray *imageNames = @[@"messagesWarning", @"messagesAccess", @"messagesConfig", @"messagesDocument", @"messagesInfo", @"messagesWarning",@"messagesError", @"messagesPanic"];
+    NSImage *warningImage = [[NSBundle bundleForClass:[self class]] imageForResource:imageNames[level]];
+    return warningImage;
+}
+
+
 + (NSImage *)imageForWarningStyle:(MGSErrorType)style
 {
-    // Note these are in order by MGSErrorType.
-    NSArray *imageNames = @[@"messagesWarning", @"messagesAccess", @"messagesConfig", @"messagesDocument", @"messagesInfo", @"messagesWarning",@"messagesError", @"messagesPanic"];
-    NSImage *warningImage = [[NSBundle bundleForClass:[self class]] imageForResource:imageNames[style]];
-    [warningImage setSize:NSMakeSize(16.0, 16.0)];
-    return warningImage;
+    return [[self class] defaultImageForWarningLevel:style];
 }
 
 
 #pragma mark - Property Accessors
 
+- (void)setErrorLineHighlightColor:(NSColor *)errorLineHighlightColor
+{
+    _errorLineHighlightColor = errorLineHighlightColor;
+}
+
+- (NSColor *)errorLineHighlightColor
+{
+    if (!_errorLineHighlightColor)
+    {
+        _errorLineHighlightColor = [NSColor colorWithCalibratedRed:1 green:1 blue:0.7 alpha:1];
+    }
+
+    return _errorLineHighlightColor;
+}
+
+
+-(void)setErrorBackgroundHighlightColor:(NSColor *)errorBackgroundHighlightColor
+{
+    _errorBackgroundHighlightColor = errorBackgroundHighlightColor;
+}
+
+- (NSColor *)errorBackgroundHighlightColor
+{
+    if (!_errorBackgroundHighlightColor)
+    {
+        _errorBackgroundHighlightColor = [NSColor orangeColor];
+    }
+
+    return _errorBackgroundHighlightColor;
+}
+
+
+-(void)setErrorForegroundHilightColor:(NSColor *)errorForegroundHilightColor
+{
+    _errorForegroundHilightColor = errorForegroundHilightColor;
+}
+
+- (NSColor *)errorForegroundHilightColor
+{
+    if (!_errorForegroundHilightColor)
+    {
+        _errorForegroundHilightColor = [NSColor whiteColor];
+    }
+
+    return _errorForegroundHilightColor;
+}
+
+
+- (void)setWarningImage:(NSImage *)warningImage
+{
+    _warningImage = warningImage;
+}
+
 - (NSImage *)warningImage
 {
-    return [[self class] imageForWarningStyle:self.warningStyle];
+    if (!_warningImage)
+    {
+        _warningImage = [[self class] defaultImageForWarningLevel:self.warningStyle];
+    }
+
+    return _warningImage;
+}
+
+
+#pragma mark - Deprecated Properties
+
+
+- (void)setWarningStyle:(MGSErrorType)style
+{
+    self.warningLevel = style;
+}
+
+- (MGSErrorType)warningStyle
+{
+    return self.warningLevel;
+}
+
+
+- (void)setHideWarning:(BOOL)hideWarning
+{
+    self.hidden = hideWarning;
+}
+
+- (BOOL)hideWarning
+{
+    return self.hidden;
+}
+
+
+-(void)setCustomBackgroundColor:(NSColor *)customBackgroundColor
+{
+    self.errorBackgroundHighlightColor = customBackgroundColor;
+}
+
+- (NSColor *)customBackgroundColor
+{
+    return self.errorBackgroundHighlightColor;
 }
 
 @end
