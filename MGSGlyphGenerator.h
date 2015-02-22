935aeee5abdaa28bb66b07a8b833cc585b1d627e
Jim Derry <balthisar@gmail.com>
2015-02-22 20:07:52 +0800

    More documentation.

--- MGSGlyphGenerator.h
+++ MGSGlyphGenerator.h
@@ -8,6 +8,10 @@
 
 #import <Cocoa/Cocoa.h>
 
+/**
+ *  This subclass of NSGlyphGenerator is used to generate glyphs for
+ *  invisible characters.
+ **/
 @interface MGSGlyphGenerator : NSGlyphGenerator <NSGlyphStorage> {
     id <NSGlyphStorage> _destination;
 }
