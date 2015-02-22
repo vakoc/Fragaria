e111df9cfc116a7b5dd01a30236307646b3c156e
Jim Derry <balthisar@gmail.com>
2015-02-22 13:22:57 +0800

    Documentation cleanup.

--- MGSFragariaFontsAndColoursPrefsViewController.h
+++ MGSFragariaFontsAndColoursPrefsViewController.h
@@ -9,8 +9,23 @@
 #import <Cocoa/Cocoa.h>
 #import "MGSFragariaPrefsViewController.h"
 
+/**
+ *  MGSFragariaFontsAndColoursPrefsViewController handles the view
+ *  provided by MGSPreferencesFontsAndColours.xib.
+ **/
+
 @interface MGSFragariaFontsAndColoursPrefsViewController : MGSFragariaPrefsViewController
 
+/**
+ *  setFontAction: responds to the "Set Font" button in order to open the
+ *  standard OS font picker.
+ **/
 - (IBAction)setFontAction:(id)sender;
+
+/**
+ *  changeFont: is sent up the responder chain by the fontManager so we have
+ *  to call this method from, e.g., the preferences window controller which has
+ *  been configured as the window delegate.
+**/
 - (void)changeFont:(id)sender;
 @end
--- MGSFragariaPrefsViewController.h
+++ MGSFragariaPrefsViewController.h
@@ -8,6 +8,18 @@
 
 #import <Cocoa/Cocoa.h>
 
+/**
+ *  MGSFragariaPrefsViewController provides a base class for other Fragaria
+ *  preferences controllers. It is not implemented directly anywhere.
+ *  @see MGSFragariaFontsAndColorsPrefsViewController.h
+ *  @see MGSFragariaTextEditingPrefsViewController.h
+ **/
+
 @interface MGSFragariaPrefsViewController : NSViewController <NSTabViewDelegate>
+
+/**
+ *  Commit edits, discarding changes on error.
+ **/
 - (BOOL)commitEditingAndDiscard:(BOOL)discard;
+
 @end
