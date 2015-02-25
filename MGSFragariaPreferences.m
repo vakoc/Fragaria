vakoc/Fragaria

commit a66f3642cf7ab3373b2732592006aea40c665f94

Documentation. Lazy controller creation.

MGSFragariaPreferences.m
@@ -8,6 +8,8 @@
 
 #import "MGSFragariaPreferences.h"
 
+#pragma mark - Global Keys for Accessing Preferences' Strings
+
 // colour prefs
 // persisted as [NSArchiver archivedDataWithRootObject:[NSColor whiteColor]]
 NSString * const MGSFragariaPrefsCommandsColourWell = @"FragariaCommandsColourWell";
@@ -76,21 +78,30 @@ NSString * const MGSFragariaPrefsTextFont = @"FragariaTextFont";
 // string
 NSString * const MGSFragariaPrefsSyntaxColouringPopUpString = @"FragariaSyntaxColouringPopUpString";
 
+
+#pragma mark - Statics used for this class
+
 static BOOL MGS_preferencesInitialized = NO;
 static id sharedInstance = nil;
 
+
+#pragma mark - MGSFragariaPreferences Implementation
+
 @implementation MGSFragariaPreferences
 
-@synthesize fontsAndColoursPrefsViewController, textEditingPrefsViewController;
+@synthesize fontsAndColoursPrefsViewController = _fontsAndColoursPrefsViewController;
+@synthesize textEditingPrefsViewController = _textEditingPrefsViewController;
+
+
+#pragma mark - Class Methods
 
 /*
- 
- - initializeValues
- 
+ *  - initializeValues
  */
 + (void)initializeValues
 {
-	if (MGS_preferencesInitialized) {
+	if (MGS_preferencesInitialized)
+    {
 		return;
 	}
     
@@ -168,10 +179,9 @@ static id sharedInstance = nil;
 	MGS_preferencesInitialized = YES;
 }
 
+
 /*
- 
- + sharedInstance
- 
+ *  + sharedInstance
  */
 + (MGSFragariaPreferences *)sharedInstance
 {
@@ -181,55 +191,47 @@ static id sharedInstance = nil;
     return sharedInstance;
 }
 
+
 /*
- 
- + allocWithZone:
- 
- alloc with zone for singleton
- 
+ *  + allocWithZone:
+ *    alloc with zone for singleton
  */
 + (id)allocWithZone:(NSZone *)zone
 {
 #pragma unused(zone)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
 	return [self sharedInstance];
+#pragma clang diagnotic pop
 }
 
-#pragma mark -
-#pragma mark Instance methods
+
+#pragma mark - Instance methods
 
 /*
- 
- - init
- 
+ *  - init
  */
 - (id)init
 {
     if (sharedInstance) return sharedInstance;
     self = [super init];
-    if (self) {
-        // load view controllers
-        textEditingPrefsViewController = [[MGSFragariaTextEditingPrefsViewController alloc] init];
-        fontsAndColoursPrefsViewController = [[MGSFragariaFontsAndColoursPrefsViewController alloc] init];
-    }
     sharedInstance = self;
     return self;
 }
 
+
 /*
- 
- - changeFont:
- 
+ *  - changeFont:
  */
 - (void)changeFont:(id)sender
 {
     /* NSFontManager will send this method up the responder chain */
-    [fontsAndColoursPrefsViewController changeFont:sender];
+    [_fontsAndColoursPrefsViewController changeFont:sender];
 }
 
+
 /*
- 
- - revertToStandardSettings:
- 
+ *  - revertToStandardSettings:
  */
 - (void)revertToStandardSettings:(id)sender
 {
@@ -237,5 +239,39 @@ static id sharedInstance = nil;
     
 	[[NSUserDefaultsController sharedUserDefaultsController] revertToInitialValues:nil];
 }
+
+
+#pragma mark - Property Accessors
+
+
+/*
+ *  - MGSFragariaTextEditingPrefsViewController:
+ *    Don't allocate these resources unless someone actually needs them.
+ */
+-(MGSFragariaTextEditingPrefsViewController *)MGSFragariaTextEditingPrefsViewController
+{
+    if (!_textEditingPrefsViewController)
+    {
+        _textEditingPrefsViewController = [[MGSFragariaTextEditingPrefsViewController alloc] init];
+    }
+
+    return _textEditingPrefsViewController;
+}
+
+
+/*
+ *  - MGSFragariaFontsAndColoursPrefsViewController:
+ *    Don't allocate these resources unless someone actually needs them.
+ */
+-(MGSFragariaFontsAndColoursPrefsViewController *)MGSFragariaFontsAndColoursPrefsViewController
+{
+    if (!_fontsAndColoursPrefsViewController)
+    {
+        _fontsAndColoursPrefsViewController = [[MGSFragariaFontsAndColoursPrefsViewController alloc] init];
+    }
+
+    return _fontsAndColoursPrefsViewController;
+}
+
 @end
 

MGSFragariaPreferences.h
@@ -7,7 +7,15 @@
  *
  */
 
-// Fragraria preference keys by type
+
+#pragma mark - Global Keys for Accessing Preferences' Strings
+
+/*
+ *  Fragaria Preference Keys By Type
+ *  @todo: rearrange by function/component.
+ *  @todo: take an iventory of all components to ensure no keys are missing.
+ */
+
 
 // color data
 // [NSArchiver archivedDataWithRootObject:[NSColor whiteColor]]
@@ -77,22 +85,69 @@ extern NSString * const MGSFragariaPrefsTextFont;
 // string
 extern NSString * const MGSFragariaPrefsSyntaxColouringPopUpString;
 
+
 #import "MGSFragariaPrefsViewController.h"
 #import "MGSFragariaFontsAndColoursPrefsViewController.h"
 #import "MGSFragariaTextEditingPrefsViewController.h"
 
-@interface MGSFragariaPreferences : NSObject {
-    
-    MGSFragariaFontsAndColoursPrefsViewController *fontsAndColoursPrefsViewController;
-    MGSFragariaTextEditingPrefsViewController *textEditingPrefsViewController;
-}
-+ (void)initializeValues;
-+ (MGSFragariaPreferences *)sharedInstance;
-- (void)changeFont:(id)sender;
-- (void)revertToStandardSettings:(id)sender;
 
-@property (readonly) MGSFragariaFontsAndColoursPrefsViewController *fontsAndColoursPrefsViewController;
-@property (readonly) MGSFragariaTextEditingPrefsViewController *textEditingPrefsViewController;
+#pragma mark - MGSFragariaPreferneces
+
+/**
+ *  MGSFragariaPreferences is responsible for registering the userDefaults for instances
+ *  of Fragaria, and also offers some ready-made viewControllers should you want to
+ *  implement them in your application.
+ **/
+@interface MGSFragariaPreferences : NSObject
+
+
+/// @name Class Methods
+
+/**
+ *  Uses registerUserDefaults to add all of Fragaria's defaults to the defaults database.
+ **/
++ (void)initializeValues;
+
+
+/**
+ *  Provides a singleton instance of MGSFragariaPreferences.
+ *  @todo: this is *only* used in the demo application. And there, it's only
+ *         use is to resolve to `revertToStandardSettigs`, which uses a
+ *         global NSUserDefaultsController to perform its action. Issue #30.
+ **/
++ (MGSFragariaPreferences *)sharedInstance DEPRECATED_MSG_ATTRIBUTE("Proposed elimination.");
+
+
+/// @name Instance Methods
+
+/**
+ *  Used by the preference view controllers to propogate the changeFont message
+ *  up the responder chain.
+ *  @param sender is the sender of the message.
+ **/
+- (void)changeFont:(id)sender DEPRECATED_MSG_ATTRIBUTE("Proposed elimination.");
+
+
+/**
+ *  Used by the preference view controllers, sets the shared userDefaultsController
+ *  back to initial values.
+ *  @param sender is the sender of the message.
+ **/
+- (void)revertToStandardSettings:(id)sender DEPRECATED_MSG_ATTRIBUTE("Proposed elimination.");
+
+
+/// @name Properties
+
+/**
+ *  Provides a ready-made instance of MGSFragariaFontsAndColoursPrefsViewController for applications.
+ **/
+@property (readonly) MGSFragariaFontsAndColoursPrefsViewController *fontsAndColoursPrefsViewController DEPRECATED_MSG_ATTRIBUTE("Proposed elimination.");
+
+
+/**
+ * Provides a ready-made instance of MGSFragariaTextEditingPrefsViewController for applications.
+ **/
+@property (readonly) MGSFragariaTextEditingPrefsViewController *textEditingPrefsViewController DEPRECATED_MSG_ATTRIBUTE("Proposed elimination.");
 
 @end
 

commit b37ab5a297831b281d03dba7de1a6ee31a3b9f91

Fix properties broken in a66f364.

MGSFragariaPreferences.m
@@ -248,7 +248,7 @@ static id sharedInstance = nil;
  *  - MGSFragariaTextEditingPrefsViewController:
  *    Don't allocate these resources unless someone actually needs them.
  */
--(MGSFragariaTextEditingPrefsViewController *)MGSFragariaTextEditingPrefsViewController
+-(MGSFragariaTextEditingPrefsViewController *)textEditingPrefsViewController
 {
     if (!_textEditingPrefsViewController)
     {
@@ -263,7 +263,7 @@ static id sharedInstance = nil;
  *  - MGSFragariaFontsAndColoursPrefsViewController:
  *    Don't allocate these resources unless someone actually needs them.
  */
--(MGSFragariaFontsAndColoursPrefsViewController *)MGSFragariaFontsAndColoursPrefsViewController
+-(MGSFragariaFontsAndColoursPrefsViewController *)fontsAndColoursPrefsViewController
 {
     if (!_fontsAndColoursPrefsViewController)
     {