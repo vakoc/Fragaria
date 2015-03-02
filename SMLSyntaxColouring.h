ffc383c24c2325399ab31b2f36469697838978c3
Daniele Cattaneo <daniele.cattaneo@alice.it>
2015-03-02 02:45:19 +0100

    Update documentation for latest changes.

--- SMLSyntaxColouring.h
+++ SMLSyntaxColouring.h
@@ -41,9 +41,11 @@ Copyright 2004-2009 Peter Borg
 
 /// @name Properties
 
-/** The owning controller for instances of this class. */
-@property (nonatomic, weak) MGSFragaria *fragaria;
+/** The MGSFragaria instance to be passed to the syntax colouring delegate
+ * as the fragaria document parameter. */
+@property (weak) MGSFragaria *fragaria;
 
+/** The layout manager to be used for setting temporary attributes. */
 @property (readonly) NSLayoutManager *layoutManager;
 
 /** The syntax definition that determines how to color the text. */
@@ -55,34 +57,36 @@ Copyright 2004-2009 Peter Borg
 /** Specifies if the syntax colourer has to be disabled or not. */
 @property (nonatomic, getter=isSyntaxColoured) BOOL syntaxColoured;
 
-/** Indicates the character ranges which colouring is valid. */
+/** Indicates the character ranges where colouring is valid. */
 @property (strong, readonly) NSMutableIndexSet *inspectedCharacterIndexes;
 
 
 /// @name Instance Methods
 
-/**
- *  Initialize a new instance using a reference to an owning Fragaria..
- *  @param fragaria The instance of fragaria associated with this instance.
- **/
+
+/** Initialize a new instance using the specified layout manager.
+ * @param lm The layout manager associated with this instance. */
 - (id)initWithLayoutManager:(NSLayoutManager *)lm;
 
-/** Recolors the invalid lines in the specified range. */
+/** Recolors the invalid characters in the specified range.
+ * @param range A character range where, when this method returns, all syntax
+ *              colouring will be guaranteed to be up-to-date. */
 - (void)recolourRange:(NSRange)range;
 
 /** Marks as invalid the colouring in the range currently visible (not clipped)
- * in the specified text view. */
+ * in the specified text view.
+ * @param textView The text view from which to get a character range. */
 - (void)invalidateVisibleRangeOfTextView:(SMLTextView *)textView;
 
-/**
- *  Invalidates the coloring of the entire document.
- **/
+/** Marks the entire colouring attributes as invalid. */
 - (void)invalidateAllColouring;
 
-/**
- *  Recolours on a portion of the document, specified by `rangeToRecolour`.
- *  @param rangeToRecolour Indicates the range to be recoloured.
- **/
+/** Forces a recolouring of the character range specified. The recolouring will
+ * be done anew even if the specified range is already valid (wholly or in
+ * part).
+ * @param rangeToRecolour Indicates the range to be recoloured.
+ * @return The range that was effectively coloured. The returned range always
+ *         contains entirely the intial range. */
 - (NSRange)recolourChangedRange:(NSRange)rangeToRecolour;
 
 
