vakoc/Fragaria

commit f6d57dbb32931d7e8901e440fba41a9acf22fb1c

Rename SMLSyntaxError's description property to errorDescription because it conflicts with NSObject's -description: method.
Breaking backcompat but this is important. Nobody should muck with the -description: method because it is special: it is used by the debugger to describe the object in a text format.

MGSSyntaxErrorController.m
@@ -159,7 +159,7 @@ static NSInteger CharacterIndexFromRowAndColumn(NSUInteger line, NSUInteger char
             // Add highlight for background
             [layoutManager addTemporaryAttribute:NSBackgroundColorAttributeName value:err.errorLineHighlightColor forCharacterRange:lineRange];
             
-            if ([err.description length] > 0)
+            if ([err.errorDescription length] > 0)
                 [layoutManager addTemporaryAttribute:NSToolTipAttributeName value:err.description forCharacterRange:lineRange];
         }
     }
@@ -250,7 +250,7 @@ static NSInteger CharacterIndexFromRowAndColumn(NSUInteger line, NSUInteger char
     NSSize balloonSize;
     NSInteger i, c;
     
-    errors = [[self errorsForLine:line] valueForKey:@"description"];
+    errors = [[self errorsForLine:line] valueForKey:@"errorDescription"];
     images = [[self errorsForLine:line] valueForKey:@"warningImage"];
     if (!(c = [errors count])) return;
 

Applications/Simple/FragariaAppDelegate.m
@@ -60,7 +60,7 @@
 
     // define a syntax error
     SMLSyntaxError *syntaxError = [SMLSyntaxError new];
-    syntaxError.description = @"Syntax errors can be defined.";
+    syntaxError.errorDescription = @"Syntax errors can be defined.";
     syntaxError.line = 1;
     syntaxError.character = 1;
     syntaxError.length = 10;

MGSFragaria Tests/MGSSyntaxErrorControllerTests.m
@@ -64,7 +64,8 @@
                                                            @"warningLevel" : @(kMGSErrorCategoryPanic)
                                                            }],
                      ];
-    self.errorController = [[MGSSyntaxErrorController alloc] initWithArray:tmp];
+    self.errorController = [[MGSSyntaxErrorController alloc] init];
+    [self.errorController setSyntaxErrors:tmp];
 }
 
 - (void)tearDown
@@ -99,7 +100,7 @@
     float result4 = [[self.errorController errorForLine:4] warningLevel];
 
     // We should get @"Sample error 3." because error level is the same, and this is the first one.
-    NSString *result37 = [[self.errorController errorForLine:37] description];
+    NSString *result37 = [[self.errorController errorForLine:37] errorDescription];
 
     XCTAssert(result4 == kMGSErrorCategoryAccess && [result37 isEqualToString:@"Sample error 3."]);
 
@@ -111,7 +112,7 @@
     SMLSyntaxError *testContent = [[self.errorController errorsForLine:4] objectAtIndex:0];
     NSInteger testQuantity = [[self.errorController errorsForLine:37] count];
 
-    XCTAssert([testContent.description isEqualToString:@"Sample error 1."] && testQuantity == 2);
+    XCTAssert([testContent.errorDescription isEqualToString:@"Sample error 1."] && testQuantity == 2);
 }
 
 

SMLSyntaxError.h
@@ -83,7 +83,7 @@ extern float const kMGSErrorCategoryDefault;  ///< warningLevel = kMGSErrorCateg
 /** The length of this error, in characters. */
 @property (nonatomic,assign) NSUInteger length;
 /** A description for this error. */
-@property (nonatomic,copy) NSString* description;
+@property (nonatomic,copy) NSString* errorDescription;
 /** Indicates whether or not this error is hidden from display. */
 @property (nonatomic,assign) BOOL hidden;
 

SMLSyntaxError.m
@@ -21,19 +21,16 @@ float const kMGSErrorCategoryDefault = 500;
 
 @implementation SMLSyntaxError
 
-// automatic
-@synthesize line, character, length, description, hidden, warningLevel;
 
 // manual
-@synthesize errorLineHighlightColor = _errorLineHighlightColor;
-@synthesize errorBackgroundHighlightColor = _errorBackgroundHighlightColor;
-@synthesize errorForegroundHighlightColor = _errorForegroundHighlightColor;
 @synthesize warningImage = _warningImage;
 
 // to be deprecated
 @synthesize code; // deprecated; separate line as reminder to remove.
 
+
 #pragma mark - Class Methods
+
 
 + (NSImage *)defaultImageForWarningLevel:(float)level
 {
@@ -69,6 +66,7 @@ float const kMGSErrorCategoryDefault = 500;
     return warningImage;
 }
 
+
 + (instancetype) errorWithDictionary:(NSDictionary *)dictionary
 {
     return [[[self class] alloc] initWithDictionary:dictionary];
@@ -77,10 +75,16 @@ float const kMGSErrorCategoryDefault = 500;
 
 #pragma mark - Instance Methods
 
+
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary
 {
     if ((self = [self init])) {
-        [self setValuesForKeysWithDictionary:dictionary];
+        NSMutableDictionary *temp;
+        
+        temp = [dictionary mutableCopy];
+        [temp setObject:[temp objectForKey:@"description"] forKey:@"errorDescription"];
+        [temp removeObjectForKey:@"description"];
+        [self setValuesForKeysWithDictionary:temp];
     }
     return self;
 }
@@ -154,5 +158,6 @@ float const kMGSErrorCategoryDefault = 500;
 {
     return self.errorLineHighlightColor;
 }
+
 
 @end

commit 76d5be871063a526e5967977ed4157def7918620

Fix tooltip bug introduced in f6d57db.

MGSSyntaxErrorController.m
@@ -160,7 +160,7 @@ static NSInteger CharacterIndexFromRowAndColumn(NSUInteger line, NSUInteger char
             [layoutManager addTemporaryAttribute:NSBackgroundColorAttributeName value:err.errorLineHighlightColor forCharacterRange:lineRange];
             
             if ([err.errorDescription length] > 0)
-                [layoutManager addTemporaryAttribute:NSToolTipAttributeName value:err.description forCharacterRange:lineRange];
+                [layoutManager addTemporaryAttribute:NSToolTipAttributeName value:err.errorDescription forCharacterRange:lineRange];
         }
     }
 }