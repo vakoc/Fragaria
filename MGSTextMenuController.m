54f1f86c5f68c2925acf77c834857b88d6993c12
Daniele Cattaneo <daniele.cattaneo@alice.it>
2015-03-03 18:44:39 +0100

    Fix bug in the legacy text menu controller.

--- MGSTextMenuController.m
+++ MGSTextMenuController.m
@@ -25,6 +25,7 @@
 #import "MGSFragaria.h"
 #import "MGSFragariaFramework.h"
 #import "SMLTextView+MGSTextActions.h"
+#import "SMLTextViewPrivate.h"
 
 
 @implementation MGSTextMenuController
@@ -113,6 +114,7 @@ static id sharedInstance = nil;
 	if (![responder isKindOfClass:[SMLTextView class]]) {
 		return NO;
 	}
+    SMLTextView *textView = responder;
 	
 	// All items who should only be active if something is selected
 	if (action == @selector(removeNeedlessWhitespaceAction:) ||
@@ -129,7 +131,7 @@ static id sharedInstance = nil;
 	}
     // Comment Or Uncomment
     else if (action == @selector(commentOrUncommentAction:) ) {
-		if ([[[[[responder fragaria] valueForKey:@"syntaxColouring"] syntaxDefinition] firstSingleLineComment] isEqualToString:@""]) {
+		if ([[textView.syntaxColouring.syntaxDefinition firstSingleLineComment] isEqualToString:@""]) {
 			enableMenuItem = NO;
 		}
 	} 
