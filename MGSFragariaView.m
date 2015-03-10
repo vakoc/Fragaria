e902f83a4501c1f3ab8869c565eb99336c0a6f08
Jim Derry <balthisar@gmail.com>
2015-03-10 21:24:42 +0800

    - Added a generic handler for returning values to objects which have bound the view. - Updated the string property as a test case for the handler. - Added a unit test to ensure that two-way bindings work. - Added a unit test to enusre the multiple bound object two-way bindings works.

--- MGSFragariaView.m
+++ MGSFragariaView.m
@@ -132,6 +132,7 @@
 - (void)setString:(NSString *)string
 {
 	self.fragaria.string = string;
+    [self propagateValue:string forBinding:NSStringFromSelector(@selector(string))];
 }
 
 - (NSString *)string
@@ -1032,6 +1033,74 @@
 - (BOOL)coloursVariables
 {
     return self.fragaria.syntaxColouring.coloursVariables;
+}
+
+
+#pragma mark - KVO/KVC/BINDING Handling
+
+
+/*
+ * - propagateValue:forBinding
+ *   courtesy of Tom Dalling.
+ */
+-(void)propagateValue:(id)value forBinding:(NSString*)binding;
+{
+    NSParameterAssert(binding != nil);
+
+    // WARNING: bindingInfo contains NSNull, so it must be accounted for
+    NSDictionary* bindingInfo = [self infoForBinding:binding];
+    if (!bindingInfo)
+    {
+        return; //there is no binding
+    }
+
+    // Apply the value transformer, if one has been set
+    NSDictionary* bindingOptions = [bindingInfo objectForKey:NSOptionsKey];
+
+    if (bindingOptions)
+    {
+        NSValueTransformer* transformer = [bindingOptions valueForKey:NSValueTransformerBindingOption];
+
+        if (!transformer || (id)transformer == [NSNull null])
+        {
+            NSString* transformerName = [bindingOptions valueForKey:NSValueTransformerNameBindingOption];
+
+            if(transformerName && (id)transformerName != [NSNull null])
+            {
+                transformer = [NSValueTransformer valueTransformerForName:transformerName];
+            }
+        }
+
+        if (transformer && (id)transformer != [NSNull null])
+        {
+            if([[transformer class] allowsReverseTransformation])
+            {
+                value = [transformer reverseTransformedValue:value];
+            }
+            else
+            {
+                NSLog(@"WARNING: binding \"%@\" has value transformer, but it doesn't allow reverse transformations in %s", binding, __PRETTY_FUNCTION__);
+            }
+        }
+    }
+
+    id boundObject = [bindingInfo objectForKey:NSObservedObjectKey];
+
+    if(!boundObject || boundObject == [NSNull null])
+    {
+        NSLog(@"ERROR: NSObservedObjectKey was nil for binding \"%@\" in %s", binding, __PRETTY_FUNCTION__);
+        return;
+    }
+
+    NSString* boundKeyPath = [bindingInfo objectForKey:NSObservedKeyPathKey];
+
+    if(!boundKeyPath || (id)boundKeyPath == [NSNull null])
+    {
+        NSLog(@"ERROR: NSObservedKeyPathKey was nil for binding \"%@\" in %s", binding, __PRETTY_FUNCTION__);
+        return;
+    }
+
+    [boundObject setValue:value forKeyPath:boundKeyPath];
 }
 
 
