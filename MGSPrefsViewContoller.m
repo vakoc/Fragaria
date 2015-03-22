4503fa2f6a314215e9ca656105aece7a543091ea
Jim Derry <balthisar@gmail.com>
2015-03-22 20:07:56 +0800

    - Simplify how properties are enabled and disabled via bindings. - Fix and issue wherein non-managed properties were still enabling controls.

--- MGSPrefsViewContoller.m
+++ MGSPrefsViewContoller.m
@@ -10,7 +10,50 @@
 #import "MGSUserDefaultsController.h"
 
 
+#pragma mark - MGSManagedPropertiesProxy Class
+
+@interface MGSManagedPropertiesProxy : NSObject
+
+@property (nonatomic, weak) MGSUserDefaultsController *propertiesController;
+@property (nonatomic, weak) MGSPrefsViewContoller *viewController;
+
+@end
+
+@implementation MGSManagedPropertiesProxy
+
+/*
+ * - init
+ *   This is a simple class and there's no need to specify multiple
+ *   classes. Simply return the value appropriate to how we init'd.
+ */
+- (instancetype)initWithViewController:(MGSPrefsViewContoller *)viewController
+{
+    if ((self = [super init]))
+    {
+        self.viewController = viewController;
+    }
+
+    return self;
+}
+
+/*
+ * - valueForKey
+ */
+-(id)valueForKey:(NSString *)key
+{
+    BOOL managesProperty = [self.propertiesController.managedProperties containsObject:key];
+
+    return @(managesProperty);
+}
+
+@end
+
+
+#pragma mark - MGSPrefsViewController
+
 @interface MGSPrefsViewContoller ()
+
+@property (nonatomic, strong) MGSManagedPropertiesProxy *propertiesProxy;
 
 @end
 
@@ -19,23 +62,38 @@
 
 
 /*
- * - viewDidLoad
+ * - init
  */
-- (void)viewDidLoad
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    [super viewDidLoad];
+    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]))
+    {
+        _propertiesProxy = [[MGSManagedPropertiesProxy alloc] initWithViewController:self];
+    }
+
+    return self;
 }
 
 
 /*
- * @property propertiesController
+ *  @property propertiesController
  */
-//- (void)setPropertiesController:(MGSUserDefaultsController *)propertiesController
-//{
-//    _propertiesController = propertiesController;
-//
-//    [self.objectController setContent:propertiesController.values];
-//}
+- (void)setPropertiesController:(MGSUserDefaultsController *)propertiesController
+{
+    [self willChangeValueForKey:@"managedProperties"];
+    _propertiesController = propertiesController;
+    self.propertiesProxy.propertiesController = propertiesController;
+    [self didChangeValueForKey:@"managedProperties"];
+}
+
+
+/*
+ * @property managedProperties
+ */
+- (id)managedProperties
+{
+    return self.propertiesProxy;
+}
 
 
 @end
