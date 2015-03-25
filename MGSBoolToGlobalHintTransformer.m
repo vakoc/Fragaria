faf4685a6b193c89c30e6f14f57061b8640d774a
Jim Derry <balthisar@gmail.com>
2015-03-25 10:59:31 +0800

    Value transformer provides text for a hint about items that are bold in the settings panels.

---
+++ MGSBoolToGlobalHintTransformer.m
@@ -0,0 +1,35 @@
+//
+//  MGSBoolToGlobalHintTransformer.m
+//  Fragaria
+//
+//  Created by Jim Derry on 3/25/15.
+//
+//
+
+#import "MGSBoolToGlobalHintTransformer.h"
+
+@implementation MGSBoolToGlobalHintTransformer
+
++ (Class)transformedValueClass
+{
+	return [NSString class];
+}
+
+
++ (BOOL)allowsReverseTransformation
+{
+	return NO;
+}
+
+
+- (id)transformedValue:(id)value
+{
+	if (value && [value boolValue])
+	{
+		return NSLocalizedString(@"Items in bold effect all of your views.",@"Hint explaining why certain items are bold.");
+	}
+	
+	return nil;
+}
+
+@end
