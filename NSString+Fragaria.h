78858101fa39d70aafd8eb03401c40afed3134e4
Daniele Cattaneo <daniele.cattaneo@alice.it>
2015-12-14 17:02:12 +0100

    Add some utility functions to NSString to aid in column calculation.

--- NSString+Fragaria.h
+++ NSString+Fragaria.h
@@ -19,10 +19,29 @@
 /** The real location of the specified character index in this line, in
  *  characters.
  *  @discussion This method takes in account the real width of tabulation
- *              characters, and ignores newlines.
+ *              characters. Newlines are treated like infinite-width tabs.
  *  @param c A character index in the line string.
  *  @param tabwidth The width of a tab. */
 - (NSUInteger)mgs_columnOfCharacter:(NSUInteger)c tabWidth:(NSUInteger)tabwidth;
+
+/** Returns the character index in this row that corresponds to the specified
+ *  column (measured in characters).
+ *  @discussion This method takes in account the real width of tabulation
+ *              characters. Newlines are treated like infinite-width tabs.
+ *  @param c A column number.
+ *  @param tabwidth The width of a tab. */
+- (NSUInteger)mgs_characterInColumn:(NSUInteger)c tabWidth:(NSUInteger)tabwidth;
+
+/** Returns the range of characters representing the line containing a given
+ *  character.
+ *  @param i A character index in the string. 
+ *  @returns If i points to a character of the string, calling this method
+ *     is equivalent to calling [self lineRangeForRange:NSMakeRange(i, 0)].
+ *     Otherwise, if i is one past the last character of the string, this
+ *     method returns the line range where that character would be placed.
+ *     In all other cases, this function returns a range with NSNotFound as
+ *     location, and 0 as length. */
+- (NSRange)mgs_lineRangeForCharacterIndex:(NSUInteger)i;
 
 
 @end
